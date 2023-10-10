12ff900c3d0c73a0049467e830f64f262ad41f92
PAULO ROBERTO BELEM DE SEIXAS <61366935268@professores.estacio.br>
2023-10-09 21:38:21 -0300

    Tela de cadastro animais

--- app/src/main/java/com/example/pupfeeder/TelaListagemAnimais.java
+++ app/src/main/java/com/example/pupfeeder/TelaListagemAnimais.java
@@ -2,13 +2,27 @@ package com.example.pupfeeder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TelaListagemAnimais extends AppCompatActivity {
 
+    Button cadastrarAnimal;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_listagem_animais);
+
+        cadastrarAnimal = (Button) findViewById(R.id.buttonCadastrarAnimal);
+
+
+        cadastrarAnimal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(TelaListagemAnimais.this, TelaCadastroAnimais.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/pupfeeder/TelaPupfeeder.java
+++ app/src/main/java/com/example/pupfeeder/TelaPupfeeder.java
@@ -2,13 +2,32 @@ package com.example.pupfeeder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TelaPupfeeder extends AppCompatActivity {
+
+    Button racoes;
+    Button animais;
+    Button alimentadores;
+    Button agendamento;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_pupfeeder);
+
+        animais = (Button) findViewById(R.id.buttonAnimais);
+
+        animais.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(TelaPupfeeder.this,TelaListagemAnimais.class));
+            }
+        });
+
+
     }
 }
